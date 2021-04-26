e264a5ea1e9d4fb7972881d3b2380be182c772ca
rclinton10 <rclinton@stanford.edu>
2021-04-25 19:00:51 -0700

    the journey map "popup" code
    images not appearing when I render

--- san-jose-digitial/src/App.js
+++ san-jose-digitial/src/App.js
@@ -4,6 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div>
+        <h1>Our Programs</h1>
+        <h2>You will gain access to job, education, 
+          healthcare, and service opportuntites!<br/>
+        The following are included in our bundle package:
+        </h2>
+      </div>
+      <div>
+        <div>
+          <h2>ACCESS PROGRAMS</h2>
+          <img src="./light_blue_circle.jpg" alt="background"/>
+          <h3>Get information on <br/>a low-cost internet <br/>package!</h3>
+        </div>
+      </div>
+      <h4>Access to devices are not guarenteed. We oeprate on a first-come first-serve basis.</h4>
+    </div>
+  );
+  /*return (
+    <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +38,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  );*/
 }
 
 export default App;
