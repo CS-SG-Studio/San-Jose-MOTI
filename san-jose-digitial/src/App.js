5b2dd675b72ca6982ece6b50773bd39880289887
rclinton10 <rclinton@stanford.edu>
2021-04-25 19:08:49 -0700

    adjusted some formatting

--- san-jose-digitial/src/App.js
+++ san-jose-digitial/src/App.js
@@ -6,24 +6,24 @@ function App() {
     <div className="App">
       <div>
         <h1>Our Programs</h1>
-        <h2>You will gain access to job, education, 
+        <h2>You will gain access to job, education,<br/>
           healthcare, and service opportuntites!<br/>
         The following are included in our bundle package:
         </h2>
       </div>
       <div>
         <div>
-          <h2>ACCESS<br/>PROGRAMS</h2>
+          <h2>ACCESS PROGRAMS</h2>
           <img src="./light_blue_circle.jpg" alt="background"/>
           <h3>Get information on<br/>a low-cost internet<br/>package!</h3>
         </div>
         <div>
-          <h2>DIGITAL<br/>LITERACY<br/>TRAININGS</h2>
+          <h2>DIGITAL LITERACY<br/>TRAININGS</h2>
           <img src="./dark_blue_circle.jpg" alt="background"/>
           <h3>Learn<br/>how to use a<br/>computer and<br/>explore the<br/>internet!</h3>
         </div>
         <div>
-          <h2>DEVICE<br/>PROGRAMS</h2>
+          <h2>DEVICE PROGRAMS</h2>
           <img src="./light_blue_circle.jpg" alt="background"/>
           <h3>Free access<br/>to computers,<br/>laptops, phones,<br/>tablets, and other<br/>devices.*</h3>
         </div>
