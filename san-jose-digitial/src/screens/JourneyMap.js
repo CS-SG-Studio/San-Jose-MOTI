5b83afe5314d3afd0c18649c13d869681d46e733
rickyhuang714 <yehtang@stanford.edu>
2021-04-26 23:31:30 +0800

    JourneyMap page created

---
+++ san-jose-digitial/src/screens/JourneyMap.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import "./main.css";
+
+function JourneyMap() {
+    return (
+        <div>
+            <h1 class="whiteheader">
+                YOUR JOURNEY
+            </h1>
+            <h3 class="whiteheader">
+                WHERE ARE YOU ON THE PATH OF DIGITAL INCLUSION?
+            </h3>
+            <div>
+                <div class="digit1">1</div>
+                <div class="circle1">
+                    <span class="text1">EXPLORE OUR PROGRAMS</span>
+                </div>
+                <div class="connect1"></div>
+            </div>
+
+
+            <div>
+                <div class="digit2">2</div>
+                <div class="circle2">
+                    <span class="text2">SEE WHAT YOU ARE ELIGIBLE FOR</span>
+                </div>
+                <div class="connect2"></div>
+            </div>
+
+
+            <div>
+                <div class="digit3">3</div>
+                <div class="circle3">
+                    <span class="text3">CONNECT WITH ORGANIZATIONS</span>
+                </div>
+                <div class="connect3"></div>
+            </div>
+
+
+            <div>
+                <div class="digit4">4</div>
+                <div class="circle4">
+                    <span class="text4">STAY IN THE LOOP</span>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+export default JourneyMap;
