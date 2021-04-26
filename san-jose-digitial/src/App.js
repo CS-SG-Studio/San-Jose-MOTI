10b7691eec462df49c2521f2d88ae875df4f0af0
rclinton10 <rclinton@stanford.edu>
2021-04-25 19:07:00 -0700

    added some of the other circles in the popup

--- san-jose-digitial/src/App.js
+++ san-jose-digitial/src/App.js
@@ -13,9 +13,19 @@ function App() {
       </div>
       <div>
         <div>
-          <h2>ACCESS PROGRAMS</h2>
+          <h2>ACCESS<br/>PROGRAMS</h2>
           <img src="./light_blue_circle.jpg" alt="background"/>
-          <h3>Get information on <br/>a low-cost internet <br/>package!</h3>
+          <h3>Get information on<br/>a low-cost internet<br/>package!</h3>
+        </div>
+        <div>
+          <h2>DIGITAL<br/>LITERACY<br/>TRAININGS</h2>
+          <img src="./dark_blue_circle.jpg" alt="background"/>
+          <h3>Learn<br/>how to use a<br/>computer and<br/>explore the<br/>internet!</h3>
+        </div>
+        <div>
+          <h2>DEVICE<br/>PROGRAMS</h2>
+          <img src="./light_blue_circle.jpg" alt="background"/>
+          <h3>Free access<br/>to computers,<br/>laptops, phones,<br/>tablets, and other<br/>devices.*</h3>
         </div>
       </div>
       <h4>Access to devices are not guarenteed. We oeprate on a first-come first-serve basis.</h4>
