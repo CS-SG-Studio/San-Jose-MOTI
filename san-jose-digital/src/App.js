5c0f77183b0042639f999c4d9b34425501011348
vic-ayres <victoriaayresibarra@gmail.com>
2021-04-27 19:40:23 -0700

    Create App.js

---
+++ san-jose-digital/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import JourneyMap from './screens/JourneyMap'
+
+function App() {
+  return (
+    <div className="App">
+      <JourneyMap />
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
