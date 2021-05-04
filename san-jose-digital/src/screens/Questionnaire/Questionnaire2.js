b420f684665ea83c19258f82b6a11d92b92a3b69
Senem Isik <77026721+SENEMISIK@users.noreply.github.com>
2021-05-04 11:41:03 -0700

    smart phone questions added

--- san-jose-digital/src/screens/Questionnaire/Questionnaire2.js
+++ san-jose-digital/src/screens/Questionnaire/Questionnaire2.js
@@ -87,18 +87,56 @@ function Questionnaire2() {
           <input type="checkbox" id="tablet" name="devicekind" value="tablet" />
           <label for="tablet">Tablet</label>
           <br />
-          <label htmlFor="deviceage">What kind of devices?</label>
-          <input type="checkbox" id="<1" name="deviceage" value="<1" />
+          <label htmlFor="deviceage">How old is the device(s)?</label>
+          <input type="radio" id="<1" name="deviceage" value="<1" />
           <label for="<1">Less than a year</label>
-          <input type="checkbox" id="1-3" name="deviceage" value="1-3" />
+          <input type="radio" id="1-3" name="deviceage" value="1-3" />
           <label for="1-3">1-3 years</label>
-          <input type="checkbox" id=">3" name="deviceage" value=">3" />
+          <input type="radio" id=">3" name="deviceage" value=">3" />
           <label for=">3">More than 3 years</label>
           <br />
           <label htmlFor="deviceamount">
             How many devices do you have in your home?
           </label>
           <input type="number" id="deviceamount" name="deviceamount"></input>
+        </div>
+        <div className="form-control">
+          <label htmlFor="smartphone">Do you have a smartphone?</label>
+          <br />
+          <input type="radio" id="yes" name="smartphone" value="yes" />
+          <label for="yes">Yes</label>
+          <br />
+          <input type="radio" id="no" name="smartphone" value="no" />
+          <label for="no">No</label> <br />
+          <label>If you have a smartphone: </label> <br />
+          <label htmlFor="internet">
+            Do you use your smartphone to connect to the Internet?
+          </label>
+          <br />
+          <input type="radio" id="yes1" name="internet" value="yes" />
+          <label for="yes1">Yes</label>
+          <input type="radio" id="no1" name="internet" value="no" />
+          <label for="no1">No</label>
+          <br />
+          <label htmlFor="carrier"> Who is your smartphone carrier? </label>
+          <input type="text" id="carrier" name="carrier"></input> <br />
+          <label htmlFor="dataplan">
+            Do you periodically buy data plans for your smartphone?
+          </label>
+          <br />
+          <input type="radio" id="yes2" name="dataplan" value="yes" />
+          <label for="yes2">Yes</label>
+          <input type="radio" id="no2" name="dataplan" value="no" />
+          <label for="no2">No</label> <br />
+          <label htmlFor="hotspot">
+            Do you ever use your smartphone as a “hotspot” to connect another
+            device to the Internet?
+          </label>
+          <br />
+          <input type="radio" id="yes3" name="hotspot" value="ye3" />
+          <label for="yes3">Yes</label>
+          <input type="radio" id="no3" name="hotspot" value="no" />
+          <label for="no3">No</label>
         </div>
       </form>
       <div>
--- san-jose-digital/src/screens/Questionnaire/Questionnaire3.js
+++ san-jose-digital/src/screens/Questionnaire/Questionnaire3.js
@@ -3,6 +3,35 @@ function Questionnaire3() {
     <>
       <container>
         <form className="form">
+          <div className="form-control">
+            <label htmlFor="household">
+              How many people luve in your home?
+            </label>
+            <input type="number" id="household" name="household"></input>
+            <label htmlFor="children">How many children do you have?</label>
+            <input type="number" id="household" name="household"></input>
+            <br />
+            <label>If you have a child/children in school: </label> <br />
+            <label htmlFor="school">
+              Does the school provide a computing device for your child/children
+              in class?
+            </label>
+            <br />
+            <input type="radio" id="yes1" name="school" value="yes" />
+            <label for="yes1">Yes</label>
+            <input type="radio" id="no1" name="school" value="no" />
+            <label for="no1">No</label>
+            <br />
+            <label htmlFor="home">
+              Is your child/children allowed to bring home the device to do
+              homework and to be used by the family?
+            </label>
+            <br />
+            <input type="radio" id="yes2" name="home" value="yes" />
+            <label for="yes2">Yes</label>
+            <input type="radio" id="no2" name="home" value="no" />
+            <label for="no2">No</label>
+          </div>
           
         </form>
       </container>
