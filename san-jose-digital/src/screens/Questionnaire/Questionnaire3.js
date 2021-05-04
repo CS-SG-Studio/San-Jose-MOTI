ac5980eff4e53225c9a6fc83aa4da9f0bb145bd8
Senem Isik <77026721+SENEMISIK@users.noreply.github.com>
2021-05-04 13:36:03 -0700

    updated questionnaire 3

--- san-jose-digital/src/screens/Questionnaire/Questionnaire3.js
+++ san-jose-digital/src/screens/Questionnaire/Questionnaire3.js
@@ -10,34 +10,136 @@ const Questionnaire3 = () => {
       <container>
         <form className="form">
           <div className="form-control">
-            <label htmlFor="contact"> What is your primary language : </label>
+            <label htmlFor="household">
+              How many people live in your home?
+            </label>
+            <input type="number" id="household" name="household"></input> <br />
+            <label htmlFor="children">How many children do you have?</label>
+            <input type="number" id="household" name="household"></input>
             <br />
-            <input type="radio" id="english" name="language" value="english" />
-            <label for="english">English</label>
+            <label>If you have a child/children in school: </label> <br />
+            <label htmlFor="school">
+              Does the school provide a computing device for your child/children
+              in class?
+            </label>
+            <input type="radio" id="yes1" name="school" value="yes" />
+            <label for="yes1">Yes</label>
+            <input type="radio" id="no1" name="school" value="no" />
+            <label for="no1">No</label>
             <br />
-            <input type="radio" id="spanish" name="language" value="spanish" />
-            <label for="spanish">Spanish</label>
+            <label htmlFor="home">
+              Is your child/children allowed to bring home the device to do
+              homework and to be used by the family?
+            </label>
+            <input type="radio" id="yes2" name="home" value="yes" />
+            <label for="yes2">Yes</label>
+            <input type="radio" id="no2" name="home" value="no" />
+            <label for="no2">No</label>
+          </div>
+          <div>
+            <label htmlFor="internetaccess">
+              Do you access the Internet at home?
+            </label>
+            <input type="radio" id="yes" name="internetaccess" value="yes" />
+            <label for="yes">Yes</label>
+            <input type="radio" id="no" name="internetaccess" value="no" />
+            <label for="no">No</label>
             <br />
+            <label>If you access the Internet at home: </label> <br />
+            <label htmlFor="cost">
+              What is the monthly cost of your Internet service?
+            </label>
+            <br />
+            <input type="radio" id="1" name="cost" value="1" />
+            <label for="1">$9.99 or less</label>
+            <input type="radio" id="2" name="cost" value="2" />
+            <label for="2">$10 - $14.99</label>
+            <input type="radio" id="3" name="cost" value="3" />
+            <label for="3">$15 - $20.99</label>
+            <input type="radio" id="4" name="cost" value="4" />
+            <label for="4">$21 or more</label> <br />
+            <label htmlFor="provider">
+              Who is your Internet Service Provider?
+            </label>
+            <input type="text" id="provider" name="provider"></input> <br />
+            <label>If you do NOT access the Internet at home: </label> <br />
+            <label for="internetuse"> Do you use the Internet at: </label>
+            <input type="radio" id="work" name="internetuse" value="work" />
+            <label for="work">Work</label>
             <input
               type="radio"
-              id="vietnamese"
-              name="language"
-              value="vietnamese"
+              id="library"
+              name="internetuse"
+              value="library"
             />
-            <label for="vietnamese">Vietnamese</label>
-            <br />
-            <input type="radio" id="other1" name="language" value="other1" />
-            <label for="other">
-              Other (please eloborate)
-              <input type="text" id="other" name="contact"></input>
+            <label for="library">Library</label>
+            <input
+              type="radio"
+              id="Friend’s Home"
+              name="internetuse"
+              value="Friend’s Home"
+            />
+            <label for="Friend’s Home">Friend’s Home</label>
+            <input type="radio" id="other" name="internetuse" value="other" />
+            <label for="other">Other</label> <br />
+            <label for="heardof">
+              Have you heard about affordable home Internet offers?
             </label>
+            <input type="radio" id="yes2" name="heardof" value="yes2" />
+            <label for="yes2">Yes</label>
+            <input type="radio" id="no2" name="heardof" value="no2" />
+            <label for="no2">No</label> <br />
+            <label for="learning">
+              Are you interested in learning about affordable home Internet
+              offers?
+            </label>
+            <input type="radio" id="yes3" name="learning" value="yes3" />
+            <label for="yes2">Yes</label>
+            <input type="radio" id="no3" name="learning" value="no3" />
+            <label for="no3">No</label>
+          </div>
+          <div>
+            <label htmlFor="experience">
+              Do you have experience using a computing device?
+            </label>
+            <input type="radio" id="yes" name="experience" value="yes" />
+            <label for="yes">Yes</label>
+            <input type="radio" id="no" name="experience" value="no" />
+            <label for="no">No</label>
+          </div>
+          <div>
+            <label htmlFor="level">
+              If you have experience using a computing device, do you consider
+              yourself to be a:
+            </label>
+            <br />
+            <input type="radio" id="beginner" name="level" value="beginner" />
+            <label for="beginner">Beginner User</label>
+            <input
+              type="radio"
+              id="intermediate"
+              name="level"
+              value="intermediate"
+            />
+            <label for="intermediate">Intermediate User</label>
+            <input type="radio" id="advanced" name="level" value="advanced" />
+            <label for="advanced">Advanced User</label>
+          </div>
+          <div>
+            <label htmlFor="classes">
+              Are you interested in learning about FREE computer classes?
+            </label>
+            <input type="radio" id="yes" name="classes" value="yes" />
+            <label for="yes">Yes</label>
+            <input type="radio" id="no" name="classes" value="no" />
+            <label for="no">No</label>
           </div>
         </form>
+        <div>
+          <Link type="submit">BACK</Link>
+          <Link type="submit">SUBMIT</Link>
+        </div>
       </container>
-      <div>
-        <Link type="submit">BACK</Link>
-        <Link type="submit">SUBMIT</Link>
-      </div>
     </Router>
   );
 }
