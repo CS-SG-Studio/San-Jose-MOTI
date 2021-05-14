2ef9a27091e66803aa3582fb5fe82c8daa115f62
habeeb <habiblah.jimoh@gmail.com>
2021-05-13 17:56:08 -0700

    radio

--- san-jose-digital/src/screens/Questionnaire/Questionnaire1.js
+++ san-jose-digital/src/screens/Questionnaire/Questionnaire1.js
@@ -5,13 +5,17 @@ function Questionnaire1() {
     <div className="container">
       <header className="header"> QUESTIONNAIRE </header>
       <p className="subheader">
-        Fill out the following questionnaire to get matched with grantee
-        organization(s).
+        Fill out the following questionnaire to get matched with grantee organization(s).
       </p>
       <form className="form">
         <div className="form-control">
           <label htmlFor="firstName"> Name: </label>
-          <input type="text" id="firstName" name="firstName" placeholder="Your name..."></input>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            placeholder="Your name..."
+          ></input>
         </div>
         <div className="form-control">
           <label htmlFor="email"> E-Mail: </label>
@@ -32,11 +36,13 @@ function Questionnaire1() {
         <div className="form-control">
           <label htmlFor="contact"> I am filling this on behalf of: </label>
           <br />
-          <input type="radio" id="contact" name="contact" value="self" />
-          <label htmlFor="contact">Myself</label>
+          <label htmlFor="contact">
+            <input type="radio" id="contact" name="contact" value="self" />
+            Myself
+          </label>
           <br />
-          <input type="radio" id="someone" name="contact" value="someone" />
           <label htmlFor="someone">
+            <input type="radio" id="someone" name="contact" value="someone" />
             Someone else (please specify)
             <input type="text" id="someone" name="contact"></input>
           </label>
@@ -50,12 +56,7 @@ function Questionnaire1() {
           <input type="radio" id="spanish" name="language" value="spanish" />
           <label htmlFor="spanish">Spanish</label>
           <br />
-          <input
-            type="radio"
-            id="vietnamese"
-            name="language"
-            value="vietnamese"
-          />
+          <input type="radio" id="vietnamese" name="language" value="vietnamese" />
           <label htmlFor="vietnamese">Vietnamese</label>
           <br />
           <input type="radio" id="other1" name="language" value="other1" />
