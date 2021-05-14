becb2361c6777270a6a292022f36d30e4edf29e6
vic-ayres <victoriaayresibarra@gmail.com>
2021-05-13 19:02:59 -0700

    Update App.js

--- san-jose-digital/src/App.js
+++ san-jose-digital/src/App.js
@@ -1,16 +1,38 @@
 // import logo from './logo.svg';
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink
+} from "react-router-dom";
+
 import JourneyMap from "./screens/JourneyMap/JourneyMap";
 import Step1 from "./screens/JourneyMap/Step1/Step1";
 import Questionnaire from "./screens/Questionnaire/Questionnaire";
+import Questionnaire2 from "./screens/Questionnaire/Questionnaire2";
+
 
 function App() {
   return (
     <div className="App">
+      
+      <Router>
       <JourneyMap />
       <Step1 />
-      <Questionnaire />
 
+      <div>
+        <NavLink to='./questionnaire'>Questionnaire</NavLink>
+        </div>
+        <Switch>
+            <Route path="/questionnaire">
+              <Questionnaire />
+            </Route>
+            <Route path="/questionnaire2">
+              <Questionnaire2 />
+            </Route>
+          </Switch>
+        </Router>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
