15a85bd951308cf1580ad18d638977f92847062f
Senem Isik <77026721+SENEMISIK@users.noreply.github.com>
2021-05-18 19:44:22 -0700

    navigation questions

--- san-jose-digital/src/App.js
+++ san-jose-digital/src/App.js
@@ -1,59 +1,12 @@
 // import logo from './logo.svg';
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  NavLink
-} from "react-router-dom";
 
 import JourneyMap from "./screens/JourneyMap/JourneyMap";
-import Step1 from "./screens/JourneyMap/Step1/Step1";
-import Questionnaire from "./screens/Questionnaire/Questionnaire";
-import Questionnaire2 from "./screens/Questionnaire/Questionnaire2";
-import Questionnaire3 from "./screens/Questionnaire/Questionnaire3";
-import Congratulations from "./screens/Questionnaire/Congratulations";
 
 function App() {
   return (
     <div className="App">
-      
-      <Router>
       <JourneyMap />
-      <Step1 />
-
-      <div>
-        <NavLink to='./questionnaire'>Questionnaire</NavLink>
-        </div>
-        <Switch>
-            <Route path="/questionnaire">
-              <Questionnaire />
-            </Route>
-            <Route path="/questionnaire2">
-              <Questionnaire2 />
-            </Route>
-            <Route path="/questionnaire3">
-              <Questionnaire3 />
-            </Route>
-            <Route path="/congratulations">
-              <Congratulations />
-            </Route>
-          </Switch>
-        </Router>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
--- san-jose-digital/src/screens/JourneyMap/JourneyMap.js
+++ san-jose-digital/src/screens/JourneyMap/JourneyMap.js
@@ -1,9 +1,19 @@
 import React, { useState } from "react";
 import ContactForm from "../LastStep/contactus";
-import Questionnaire1 from "../Questionnaire/Questionnaire1";
 import "./JourneyMapStyle.css";
-import Step1 from "./Step1/Step1.js";
-// import ContactForm "../LastStep/contactus";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink
+} from "react-router-dom";
+
+import Step1 from "./Step1/Step1"
+import Questionnaire from "../Questionnaire/Questionnaire"
+import Questionnaire2 from "../Questionnaire/Questionnaire2"
+import Questionnaire3 from "../Questionnaire/Questionnaire3"
+import Congratulations from "../Questionnaire/Congratulations";
 
 function JourneyMap() {
   const [isOpen, setIsOpen] = useState(false);
@@ -25,74 +35,95 @@ function JourneyMap() {
   };
 
   return (
-    <div className="page">
-      <div>
-        <h1 className="whiteheader" style={{ fontSize: "3.5rem" }}>
-          YOUR JOURNEY
-        </h1>
-        <h3 className="whiteheader" style={{ fontSize: "2rem" }}>
-          WHERE ARE YOU ON THE PATH OF DIGITAL INCLUSION?
-        </h3>
+    <Router>
+      <div className="page">
+        <div>
+          <h1 className="whiteheader" style={{ fontSize: "3.5rem" }}>
+            YOUR JOURNEY
+          </h1>
+          <h3 className="whiteheader" style={{ fontSize: "2rem" }}>
+            WHERE ARE YOU ON THE PATH OF DIGITAL INCLUSION?
+          </h3>
+        </div>
+        <div>
+          <div className="stack">
+            <div className="digit">1</div>
+            <div className="on-right">
+              <div className="circles first" onClick={togglePopup}>
+                <span className="instruction" onClick={togglePopup}>
+                  EXPLORE OUR PROGRAMS
+                </span>
+              </div>
+              <div className="connector first"></div>
+            </div>
+          </div>
+
+          <div className="stack">
+            <div className="digit">2</div>
+            <div className="on-right">
+              <div className="circles second" onClick={questionnaireStep}>
+                <span className="instruction" onClick={questionnaireStep}>
+                  <div>
+                    <NavLink to="./questionnaire">
+                      SEE WHAT YOU ARE ELIGIBLE FOR
+                    </NavLink>
+                  </div>
+                </span>
+              </div>
+              <div className="connector second1"></div>
+              {openQuestionnaire && (
+                <Switch>
+                  <Route path="/questionnaire">
+                    <Questionnaire />
+                  </Route>
+                  <Route path="/questionnaire2">
+                    <Questionnaire2 />
+                  </Route>
+                  <Route path="/questionnaire3">
+                    <Questionnaire3 />
+                  </Route>
+                  <Route path="/congratulations">
+                    <Congratulations />
+                  </Route>
+                </Switch>
+              )}
+
+              <div className="connector second2"></div>
+            </div>
+          </div>
+
+          <div className="stack">
+            <div className="digit">3</div>
+            <div className="on-right">
+              <div className="circles third">
+                <span className="instruction">
+                  LEARN MORE ABOUT THE GRANTEES
+                </span>
+              </div>
+              <div className="connector third"></div>
+            </div>
+          </div>
+
+          <div className="stack">
+            <div className="digit">4</div>
+            <div className="on-right">
+              <div className="circles fourth" onClick={contactStep}>
+                <span className="instruction" onClick={contactStep}>
+                  STAY IN THE LOOP
+                </span>
+              </div>
+              {openContactForm && (
+                <>
+                  <div className="connector fourth"></div>
+                  <ContactForm />
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+        {isOpen && <Step1 handleClose={togglePopup} />}
       </div>
-      <div>
-        <div className="stack">
-          <div className="digit">1</div>
-          <div className="on-right">
-            <div className="circles first" onClick={togglePopup}>
-              <span className="instruction" onClick={togglePopup}>
-                EXPLORE OUR PROGRAMS
-              </span>
-            </div>
-            <div className="connector first"></div>
-          </div>
-        </div>
-
-        <div className="stack">
-          <div className="digit">2</div>
-          <div className="on-right">
-            <div className="circles second" onClick={questionnaireStep}>
-              <span className="instruction" onClick={questionnaireStep}>
-                SEE WHAT YOU ARE ELIGIBLE FOR
-              </span>
-            </div>
-            <div className="connector second1"></div>
-            {openQuestionnaire && (
-                <Questionnaire1 />
-            )}
-            
-            <div className="connector second2"></div>
-          </div>
-        </div>
-
-        <div className="stack">
-          <div className="digit">3</div>
-          <div className="on-right">
-            <div className="circles third">
-              <span className="instruction">LEARN MORE ABOUT THE GRANTEES</span>
-            </div>
-            <div className="connector third"></div>
-          </div>
-        </div>
-
-        <div className="stack">
-          <div className="digit">4</div>
-          <div className="on-right">
-            <div className="circles fourth" onClick={contactStep}>
-              <span className="instruction" onClick={contactStep}>
-                STAY IN THE LOOP
-              </span>
-            </div>
-            {openContactForm && (
-              <>
-                <div className="connector fourth"></div>
-                <ContactForm />
-              </>
-            )}
-          </div>
-        </div>
-      </div>
-      {isOpen && <Step1 handleClose={togglePopup} />}
-    </div>
+    </Router>
   );
 }
 export default JourneyMap;
