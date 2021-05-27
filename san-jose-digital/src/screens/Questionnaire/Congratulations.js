9fa6b46e9f365a728b5e8cc7de0aab88730a30bc
Senem Isik <77026721+SENEMISIK@users.noreply.github.com>
2021-05-27 16:20:54 -0700

    editted congratulation spage

--- san-jose-digital/src/screens/Questionnaire/Congratulations.js
+++ san-jose-digital/src/screens/Questionnaire/Congratulations.js
@@ -4,17 +4,26 @@ import './CongratulationStyles.css'
 function Congratulations() {
   return (
     <div className="congrats-page-styling">
-      <h1 style={{ fontSize: "3rem" }}>
-        CONGRATULATIONS!
-      </h1>
-      <p style={{ fontSize: "1.5rem" }}>
+      <h1 style={{ fontSize: "1.75rem" }}>CONGRATULATIONS!</h1>
+      <p style={{ fontSize: "1.15rem" }}>
         Thank you for filling out the questionnaire!
         <br />
-        We will be in touch with you soon!
+        Your information has been passed on to grantees.
+        <br />
+        <br />
+        They will be in touch with you soon!
       </p>
-      <p style={{ fontSize: "1.5rem" }}>
-        In the meantime, please click 
-        <a href="https://www.sjdigitalinclusion.org/grantees" rel="noreferrer" target="_blank" className="link"> here </a>  
+      <p style={{ fontSize: "1.15rem" }}>
+        In the meantime, please <br /> click
+        <a
+          href="https://www.sjdigitalinclusion.org/grantees"
+          rel="noreferrer"
+          target="_blank"
+          className="link"
+        >
+          {" "}
+          here{" "}
+        </a>
         to see the full list of our grantees!
       </p>
     </div>
