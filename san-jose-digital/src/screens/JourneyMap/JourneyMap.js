2199110b7da563efeb8af607d8ff19831388c553
habeeb <habiblah.jimoh@gmail.com>
2021-05-27 16:28:13 -0700

    step2

--- san-jose-digital/src/screens/JourneyMap/JourneyMap.js
+++ san-jose-digital/src/screens/JourneyMap/JourneyMap.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import ContactForm from "../Step4Contact/contactus";
 import "./JourneyMapStyle.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import Step1 from "./Step1/Step1";
+import Step1 from "./Step1Program/program";
 import Congratulations from "../Questionnaire/Congratulations";
 import Questionnaire from "../Questionnaire/Questionnaire";
 import WelcomePage from "../WelcomePage/welcomePage";
