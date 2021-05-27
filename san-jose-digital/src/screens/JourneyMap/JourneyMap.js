9b0e117c92cf0e82a46ba5888aadac22e1470b4e
habeeb <habiblah.jimoh@gmail.com>
2021-05-27 10:33:35 -0700

    YESgit pull

--- san-jose-digital/src/screens/JourneyMap/JourneyMap.js
+++ san-jose-digital/src/screens/JourneyMap/JourneyMap.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 import ContactForm from "../LastStep/contactus";
 import "./JourneyMapStyle.css";
-import { BrowserRouter as Router,
-} from "react-router-dom"; 
-import Step1 from "./Step1/Step1"
-import Congratulations from "../Questionnaire/Congratulations"
-import Questionnaire from "../Questionnaire/Questionnaire"; 
+import { BrowserRouter as Router } from "react-router-dom";
+import Step1 from "./Step1/Step1";
+import Congratulations from "../Questionnaire/Congratulations";
+import Questionnaire from "../Questionnaire/Questionnaire";
 
 // change
 
@@ -24,7 +23,7 @@ function JourneyMap() {
   const [openCongratulationsPage, setCongratulationsPage] = useState(false);
   const congratulationsStep = () => {
     setCongratulationsPage(!openCongratulationsPage);
-  }
+  };
 
   const [openContactForm, setContactForm] = useState(false);
   const contactStep = () => {
@@ -39,7 +38,6 @@ function JourneyMap() {
           <h6 className="subtitle">WHERE ARE YOU ON THE PATH OF DIGITAL INCLUSION?</h6>
         </h1>
       </div>
-
       <div className="page">
         <div className="stack">
           <div className="on-right">
@@ -51,32 +49,25 @@ function JourneyMap() {
           </div>
         </div>
 
-          <div className="stack">
-            <div className="digit">2</div>
-            <div className="on-right">
-              <div className="circles second" onClick={questionnaireStep}>
-                <span className="instruction" onClick={questionnaireStep}>
-                  SEE WHAT YOU ARE ELIGIBLE FOR
-                </span>
-              </div>
-              <div className="connector second1"></div>
-              {openQuestionnaire && <Questionnaire/>}
-              <div className="connector second2"></div>
+        <div className="stack">
+          <div className="on-right">
+            <div className="circles second" onClick={questionnaireStep}>
+              <span className="instruction" onClick={questionnaireStep}>
+                SEE WHAT YOU ARE ELIGIBLE FOR
+              </span>
             </div>
+            <div className="popups">{openQuestionnaire && <Questionnaire />}</div>
           </div>
         </div>
-          <div className="stack">
-            <div className="digit">3</div>
-            <div className="on-right">
-              <div className="circles third" onClick={congratulationsStep}>
-                <span className="instruction" onClick={congratulationsStep}>
-                  LEARN MORE ABOUT THE GRANTEES
-                </span>
-              </div>
-              <div className="connector third1"></div>
-              {openCongratulationsPage && <Congratulations/>}
-              <div className="connector third2"></div>
+
+        <div className="stack">
+          <div className="on-right">
+            <div className="circles third" onClick={congratulationsStep}>
+              <span className="instruction" onClick={congratulationsStep}>
+                LEARN MORE ABOUT THE GRANTEES
+              </span>
             </div>
+            <div className="popups">{openCongratulationsPage && <Congratulations />}</div>
           </div>
         </div>
 
@@ -89,17 +80,13 @@ function JourneyMap() {
             </div>
             <div className="popups">
               {openContactForm && (
-                <>
-                  <div className="connector fourth"></div>
                   <ContactForm />
-                  {/* add another connector or some space at bottom after */}
-                </>
               )}
             </div>
           </div>
         </div>
-        {isOpen && <Step1 handleClose={togglePopup} />}
       </div>
+      <div>{isOpen && <Step1 handleClose={togglePopup} />}</div>
     </Router>
   );
 }
