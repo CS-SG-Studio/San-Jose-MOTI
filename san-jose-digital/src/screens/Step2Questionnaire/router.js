94d727edb3ac08d855eb28e71dce8496c2d94b13
habeeb <habiblah.jimoh@gmail.com>
2021-05-27 17:30:38 -0700

    folder change 2

---
+++ san-jose-digital/src/screens/Step2Questionnaire/router.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+  Switch,
+  Route
+} 
+from 'react-router-dom';
+import Questionnaire from './Questionnaire';
+import Questionnaire2 from './Questionnaire2';
+import Questionnaire3 from './Questionnaire3';
+import Congratulations from './Congratulations';
+
+// function Questionnaire() {
+const router = () => {
+  return (
+        <Switch>
+            <Route path="/questionnaire">
+                <Questionnaire/>
+            </Route>
+            <Route path="/questionnaire2">
+              <Questionnaire2 />
+            </Route>
+            <Route path="/questionnaire3">
+              <Questionnaire3 />
+            </Route>
+            <Route path="/congratulations">
+              <Congratulations />
+            </Route>
+        </Switch>
+
+  );
+}
+
+export default router;
---
+++ san-jose-digital/src/screens/Step2Questionnaire/step1.js
@@ -0,0 +1,84 @@
+function Step1(props) {
+  if (props.currentStep !== 1) {
+    return null
+  } 
+  return(
+    <container>
+    <div className="form-group">
+        <label htmlFor="name">Name</label>
+        <input
+            className="form-control"
+            id="name"
+            name="name"
+            type="text"
+            placeholder="Enter name"
+            value={props.name}
+            onChange={props.handleChange}
+        />
+    </div>
+    <div className="form-group">
+        <label htmlFor="email">Email</label>
+        <input
+            className="form-control"
+            id="email"
+            name="email"
+            type="text"
+            placeholder="Enter email"
+            value={props.email}
+            onChange={props.handleChange}
+        />
+    </div>
+    <div className="form-group">
+        <label htmlFor="phone">Phone number</label>
+        <input
+            className="form-control"
+            id="phone"
+            name="phone"
+            type="text"
+            placeholder="Enter phone number"
+            value={props.phone}
+            onChange={props.handleChange}
+        />
+    </div>
+    <div className="form-group">
+        <label htmlFor="address">Address</label>
+        <input
+            className="form-control"
+            id="address"
+            name="address"
+            type="textarea"
+            placeholder="Enter address"
+            value={props.address}
+            onChange={props.handleChange}
+            />
+    </div>
+    <div className="form-group">
+        <label htmlFor="zip_code">Zip Code</label>
+        <input
+            className="form-control"
+            id="zip_code"
+            name="zip_code"
+            type="text"
+            placeholder="Enter zip code"
+            value={props.zip_code}
+            onChange={props.handleChange}
+            />
+    </div>
+    <div className="form-group">
+        <label htmlFor="identity">I am filling this on behalf of</label>
+        <select 
+            className="form-control" 
+            id="identity" 
+            name="identity" 
+            value={props.identity}
+            onChange={props.handleChange}
+        >
+            <option name="self">Myself</option>
+            <option name="else">Someone else</option>
+        </select>
+    </div>
+    </container>
+  );
+}
+
+export default Step1;
