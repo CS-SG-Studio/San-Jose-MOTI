a5c4c5f2d89f3650bea6d7554bf163c5fbf1859f
rclinton10 <rclinton@stanford.edu>
2021-05-31 00:06:51 -0700

    .

--- san-jose-digital/src/screens/JourneyMap/Step1/Step1.js
+++ san-jose-digital/src/screens/JourneyMap/Step1/Step1.js
@@ -9,35 +9,40 @@ const Step1 = (props) => {
       </span>
       <div className="centering-all">
         <div className="header">
+<<<<<<< Updated upstream:san-jose-digital/src/screens/JourneyMap/Step1/Step1.js
           <h1 className="title">Our Programs</h1>
           <h2 className="title2">
             You will gain access to job, education, <br />
             healthcare, and service opportunities! <br />
             The following are included in our bundle package:
+=======
+          <h1 className="steptitle">Our Programs</h1>
+          <h2 className="steptitle2">
+            Our bundle package includes internet packages, digital literacy training, and access to free devices.
+            <br/>Click on the circles below for more information.
+>>>>>>> Stashed changes:san-jose-digital/src/screens/Step1Program/program.js
           </h2>
         </div>
 
         <div className="body">
           {/*Access Programs*/}
           <div className="program">
-            <h2 className="pillars">ACCESS PROGRAMS</h2>
-            <div className="circle">
+            {/* <h2 className="pillars">ACCESS PROGRAMS</h2> */}
+            <div className="circle title1">
               <a
                 href="https://www.everyoneon.org"
                 target="_blank"
                 rel="noreferrer"
                 style={{ textDecoration: "none" }}
               >
-                <h3 className="description">
-                  Click here to get information on a low-cost internet package!
-                </h3>
+                <h3 className="description"></h3>
               </a>
             </div>
           </div>
 
           {/*Digital Literacy Trainings*/}
           <div className="program">
-            <h2 className="pillars">DIGITAL LITERACY TRAININGS</h2>
+            {/* <h2 className="pillars">DIGITAL LITERACY TRAININGS</h2> */}
             <div className="circle">
               <a
                 href="https://www.google.com" /* not sure what website we want here */
@@ -46,11 +51,12 @@ const Step1 = (props) => {
                 style={{ textDecoration: "none" }}
               >
                 <h3 className="description">
-                  Learn
+                  {/* Learn
                   <br />
                   how to use a computer and explore the
                   <br />
-                  internet!
+                  internet! */}
+                  DIGITAL LITERACY TRAININGS
                 </h3>
               </a>
             </div>
@@ -58,7 +64,7 @@ const Step1 = (props) => {
 
           {/*Device Programs*/}
           <div className="program">
-            <h2 className="pillars">DEVICE PROGRAMS</h2>
+            {/* <h2 className="pillars">DEVICE PROGRAMS</h2> */}
             <div className="circle">
               <a
                 href="https://www.google.com" /* not sure what website we want here */
@@ -67,9 +73,10 @@ const Step1 = (props) => {
                 style={{ textDecoration: "none" }}
               >
                 <h3 className="description">
-                  Free access
+                  {/* Free access
                   <br />
-                  to computers, laptops, phones, tablets, and other devices.*
+                  to computers, laptops, phones, tablets, and other devices.* */}
+                  FREE DEVICE PROGRAMS*
                 </h3>
               </a>
             </div>
