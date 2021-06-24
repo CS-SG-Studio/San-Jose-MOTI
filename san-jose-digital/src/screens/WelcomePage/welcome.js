28a78da07134f9c7d5b4000d6725afe41858e9b8
habeeb <habiblah.jimoh@gmail.com>
2021-06-23 22:34:37 -0400

    second welcome

--- san-jose-digital/src/screens/WelcomePage/welcome.js
+++ san-jose-digital/src/screens/WelcomePage/welcome.js
@@ -3,68 +3,36 @@ import "./welcomeStyles.css";
 
 const WelcomePage = (props) => {
   return (
-    <>
-      <div className="welcomePopup">
-        <h2>ARE YOU LOOKING FOR...</h2>
-        <div className="looking-for">
-          <div className="welcome-circle">
-            <p className="welcome-circle-description">Low-Cost Internet Packages?</p>
-          </div>
-          <div className="welcome-circle">
-            <p className="welcome-circle-description">Computer and Internet Lessons?</p>
-          </div>
-          <div className="welcome-circle">
-            <p className="welcome-circle-description">Free Access to Digital Devices?</p>
-          </div>
-        </div>
-        <h3 className="welcome-description">
-          YES? You are in the right place! <br />
-          <button className="button">
-            <a href="#step1" rel="noreferrer">
-              <h3 className="startNow">CLICK HERE TO START YOUR JOURNEY</h3>
-            </a>
-          </button>
+    <div className="welcomePopup">
+      <h2>
+        ARE YOU LOOKING FOR...
+        <h3 className="welcome-subtitle">
+          Low-Cost Internet Packages, Computer and Internet Lessons, or Free Access to
+          Digital Devices?
         </h3>
-        <p className="welcome-description">
-          NO? Scroll down to
-          <a
-            href="#step4"
-            className="link"
-            rel="noreferrer"
-            onClick={props.setContactForm}
-          >
-            Step 4
-          </a>
-          to reach out to us with any questions.
-        </p>
+      </h2>
+      <div className="choice">
+        <a
+          href="#step1"
+          rel="noreferrer"
+        >
+          <button className="yes-button">
+            <h3 className="yes-text">YES? CLICK HERE TO START YOUR JOURNEY</h3>
+          </button>
+        </a>
+        <a
+          href="#step4"
+          rel="noreferrer"
+          onClick={props.setContactForm}
+        >
+          <button className="no-button">
+            <h3 className="no-text">
+              NO? Click here to reach out to us with any questions
+            </h3>
+          </button>
+        </a>
       </div>
-      <div className="welcomePopup">
-        <h2>
-          ARE YOU LOOKING FOR...
-          <h3 className="welcome-subtitle">
-            Low-Cost Internet Packages, Computer and Internet Lessons, or Free Access to
-            Digital Devices?
-          </h3>
-        </h2>
-        <div className="choice">
-          <a
-            href="#step1"
-            rel="noreferrer">
-            <button className="yes">
-              <h3 className="yes-text">YES? CLICK HERE TO START YOUR JOURNEY</h3>
-            </button>
-          </a>
-          <a
-            href="#step4"
-            rel="noreferrer"
-            onClick={props.setContactForm}>
-            <button className="no">
-              <h3 className="no-text">NO? Click here to reach us with any questions</h3>
-            </button>
-          </a>
-        </div>
-      </div>
-    </>
+    </div>
   );
 };
 
