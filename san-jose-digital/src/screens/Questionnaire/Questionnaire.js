a0ee42d9e06934c3127f49c562ee27a68f2dfea6
habeeb <habiblah.jimoh@gmail.com>
2021-07-09 03:00:19 -0400

    favicon

--- san-jose-digital/src/screens/Questionnaire/Questionnaire.js
+++ san-jose-digital/src/screens/Questionnaire/Questionnaire.js
@@ -1,23 +1,22 @@
-import React, { useState } from 'react';
-import Checkbox from './Checkbox';
-import Step1 from './step1';
-import Step2 from './step2';
-import Step3 from './step3';
-import './QuestionnaireStyles.css';
-import Congratulations from './Congratulations'
-
+import React, { useState } from "react";
+import Checkbox from "./Checkbox";
+import Step1 from "./step1";
+import Step2 from "./step2";
+import Step3 from "./step3";
+import "./questionnaireStyles.css";
+import Congratulations from "./Congratulations";
 
 // rename this!
 // TODO: figure out how to make currentStep based on total number of pages of questionnaire
 const Questionnairee = () => {
   const programs = [
-    'Child Enrolled in the NSLP (National School Lunch Program)',
-    'CalFresh (food stamps) or Supplemental Nutrition Assistance Program (SNAP) Recipient'
+    "Child Enrolled in the NSLP (National School Lunch Program)",
+    "CalFresh (food stamps) or Supplemental Nutrition Assistance Program (SNAP) Recipient",
   ];
 
   const componentWillMount = () => {
     this.selectedCheckboxes = new Set();
-  }
+  };
 
   const toggleCheckbox = (label) => {
     if (this.selectedCheckboxes.has(label)) {
@@ -25,19 +24,13 @@ const Questionnairee = () => {
     } else {
       this.selectedCheckboxes.add(label);
     }
-  }
+  };
 
   const createCheckbox = (label) => (
-    <Checkbox
-      label={label}
-      handleCheckboxChange={toggleCheckbox}
-      key={label}
-    />
-  )
+    <Checkbox label={label} handleCheckboxChange={toggleCheckbox} key={label} />
+  );
 
-  const createCheckboxes = () => (
-    programs.map(createCheckbox)
-  )
+  const createCheckboxes = () => programs.map(createCheckbox);
 
   const [data, setData] = useState({
     // STEP1
@@ -50,19 +43,19 @@ const Questionnairee = () => {
     identity: "",
     language: "",
 
-      // STEP2
-      programs,
-      device: "",
-      laptop: "",
-      desktop: "",
-      tablet: "",
-      deviceFollowUp: "",
-      deviceAmount: "",
-      smartphone: "",
-      connectsToInternet: "",
-      carrier: "",
-      dataPlans: "",
-    
+    // STEP2
+    programs,
+    device: "",
+    laptop: "",
+    desktop: "",
+    tablet: "",
+    deviceFollowUp: "",
+    deviceAmount: "",
+    smartphone: "",
+    connectsToInternet: "",
+    carrier: "",
+    dataPlans: "",
+
     // STEP3
     familySize: "",
     schoolDevice: "",
@@ -89,9 +82,18 @@ const Questionnairee = () => {
     identity,
     language,
 
-      // STEP2
-      device, laptop, desktop, tablet, deviceFollowUp, deviceAmount, 
-      smartphone, connectsToInternet, carrier, dataPlans, hotspot,
+    // STEP2
+    device,
+    laptop,
+    desktop,
+    tablet,
+    deviceFollowUp,
+    deviceAmount,
+    smartphone,
+    connectsToInternet,
+    carrier,
+    dataPlans,
+    hotspot,
 
     // STEP3
     familySize,
@@ -114,27 +116,28 @@ const Questionnairee = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await fetch('https://v1.nocodeapi.com/rachelclinton/google_sheets/MfimgcBbjWzzHVku?tabId=Sheet1', 
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify([
-          [name, email, phone, address, identity, new Date().toLocaleString()]
-        ]) // content that will be pushed to the Google Sheets
-      }
+      const response = await fetch(
+        "https://v1.nocodeapi.com/rachelclinton/google_sheets/MfimgcBbjWzzHVku?tabId=Sheet1",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify([
+            [name, email, phone, address, identity, new Date().toLocaleString()],
+          ]), // content that will be pushed to the Google Sheets
+        }
       );
       await response.json();
-      setData({ ...data, name:'', email:'', phone:'', address: '', identity: ''});
-      alert("Form submitted!!")
-    } catch(err) {
+      setData({ ...data, name: "", email: "", phone: "", address: "", identity: "" });
+      alert("Form submitted!!");
+    } catch (err) {
       console.log(err);
     }
     // for (const checkbox of this.selectedCheckboxes) {
     //   console.log(checkbox, 'is selected.');
     // }
-  }
+  };
 
   const _next = () => {
     let newStep = currentStep;
@@ -183,63 +186,60 @@ const Questionnairee = () => {
 
   return (
     <div className="form-styling">
-        <React.Fragment>
-          <h1>Welcome to the Questionnaire!</h1>
-          <p>Step {currentStep} </p>
+      <React.Fragment>
+        <h1>Welcome to the Questionnaire!</h1>
+        <p>Step {currentStep} </p>
 
-          <form onSubmit={handleSubmit}>
-            <Step1
-              currentStep={currentStep}
-              handleChange={handleChange}
-              name={name}
-              email={email}
-              phone={phone}
-              address={address}
-              zip_code={zip_code}
-              identity={identity}
-              language={language}
-            />
-            <Step2
-              currentStep={currentStep}
-              handleChange={handleChange}
-              programs={programs}
-              device={device}
-              desktop={desktop}
-              tablet={tablet}
-              laptop={laptop}
-              deviceFollowUp={deviceFollowUp}
-              deviceAmount={deviceAmount}
-              smartphone={smartphone}
-              connectsToInternet={connectsToInternet}
-              carrier={carrier}
-              dataPlans={dataPlans}
-              hotspot={hotspot}
-            />
-            <Step3
-              currentStep={currentStep}
-              handleChange={handleChange}
-              familySize={familySize}
-              schoolDevice={schoolDevice}
-              bringDeviceHome={bringDeviceHome}
-              homeInternet={homeInternet}
-              costOfInternet={costOfInternet}
-              internetProvider={internetProvider}
-              whereInternetIsAccessed={whereInternetIsAccessed}
-              interestedInHomeInternet={interestedInHomeInternet}
-              experienceUsingComputer={experienceUsingComputer}
-              amountOfExperience={amountOfExperience}
-              interestedInFreeClasses={interestedInFreeClasses}
-            />
-            {previousButton()}
-            {nextButton()}
-          </form>
-        </React.Fragment>
-      </div>
-  )
-}
-
-
-
+        <form onSubmit={handleSubmit}>
+          <Step1
+            currentStep={currentStep}
+            handleChange={handleChange}
+            name={name}
+            email={email}
+            phone={phone}
+            address={address}
+            zip_code={zip_code}
+            identity={identity}
+            language={language}
+          />
+          <Step2
+            currentStep={currentStep}
+            handleChange={handleChange}
+            programs={programs}
+            device={device}
+            desktop={desktop}
+            tablet={tablet}
+            laptop={laptop}
+            deviceFollowUp={deviceFollowUp}
+            deviceAmount={deviceAmount}
+            smartphone={smartphone}
+            connectsToInternet={connectsToInternet}
+            carrier={carrier}
+            dataPlans={dataPlans}
+            hotspot={hotspot}
+          />
+          <Step3
+            currentStep={currentStep}
+            handleChange={handleChange}
+            familySize={familySize}
+            schoolDevice={schoolDevice}
+            bringDeviceHome={bringDeviceHome}
+            homeInternet={homeInternet}
+            costOfInternet={costOfInternet}
+            internetProvider={internetProvider}
+            whereInternetIsAccessed={whereInternetIsAccessed}
+            interestedInHomeInternet={interestedInHomeInternet}
+            experienceUsingComputer={experienceUsingComputer}
+            amountOfExperience={amountOfExperience}
+            interestedInFreeClasses={interestedInFreeClasses}
+          />
+          {previousButton()}
+          {nextButton()}
+        </form>
+      </React.Fragment>
+    </div>
+  );
+};
 
 // class Questionnairee extends React.Component {
 //   constructor(props) {
